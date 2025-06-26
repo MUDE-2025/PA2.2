6fef2714acec16fd1f5d8d8ae7ddb5c2d7ad3fd0
Frans van der Meer <f.p.vandermeer@tudelft.nl>
2025-06-26 11:09:16 +0200

    Mesh utils to py file + Checks on results

---
+++ mesh_utils.py
@@ -0,0 +1,43 @@
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+
+def readGmsh(filename):
+    """Read GMSH mesh file and return nodes and elements"""
+    if not filename.endswith('.msh'):
+        raise RuntimeError('Unexpected mesh file extension')
+    nodes = []
+    elems = []
+    parse_nodes = False
+    parse_elems = False
+    
+    with open(filename) as msh:
+        for line in msh:
+            sp_line = line.split()
+            if '$Nodes' in line:
+                parse_nodes = True
+            elif '$Elements' in line:
+                parse_nodes = False
+                parse_elems = True
+            elif parse_nodes and len(sp_line) > 1:
+                coords = np.array(sp_line[1:], dtype=np.float64)
+                nodes.append(coords)
+            elif parse_elems and len(sp_line) > 1:
+                eltype = int(sp_line[1])
+                inodes = np.array(sp_line[3 + int(sp_line[2]):], dtype=np.int32) - 1
+                elems.append(inodes)
+    return np.array(nodes), np.array(elems)
+
+def plotMeshes(filenames):
+    fig = plt.figure(figsize=(12,12))
+    ncol = 1 + (len(filenames) > 1)
+    nrow = math.ceil(len(filenames)/ncol)
+    print('plotmeshes', nrow, ncol)
+    for idx, filename in enumerate(filenames):
+        nodes, elems = readGmsh(filename)
+        ax = fig.add_subplot(nrow, ncol, idx+1)
+        ax.triplot(nodes[:,0], nodes[:,1], elems, 'k-', lw=0.2)
+        ax.set_title(filename[:-4])
+        plt.axis('equal')
+        plt.axis('off')
+
