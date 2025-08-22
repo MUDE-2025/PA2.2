1cca3143b282e57aa55bd367c592fc3e86644535
Tom van Woudenberg <t.r.vanwoudenberg@tudelft.nl>
2025-08-22 12:11:54 +0200

    Update test_notebook.py

--- test_notebook.py
+++ test_notebook.py
@@ -2,9 +2,8 @@ from testbook import testbook
 
 def test_values():
     with testbook('PA_2_2_sparse_v_dense.ipynb', execute=True) as tb:
-        # first check should be if "results" exists at all, what happens if it is not there?
 
-        results = tb.ref("results")
+        results = tb.value("list(results)")
         
         assert len(results) > 2
         assert 'sparse_solve_time' in results[2]
